d4fb47d74496baa86b2995bf80b367ad5d6a512d
ryoh908 <ryoh908@gmail.com>
2025-04-06 15:54:48 +0900

    "Added cp _append.csv and deleted spark.DataFrame for creating a csv file"

--- db-notebooks/Chapter-03/00 - Chapter Initialization.py
+++ db-notebooks/Chapter-03/00 - Chapter Initialization.py
@@ -92,6 +92,14 @@ try:
 except:
     print("ファイルが存在しません。dbfs:/FileStore/tables/dataへYellowTaxisLargeAppend.csvファイルをアップロードしてください。")
 
+
+# COMMAND ----------
+
+try:
+    dbutils.fs.cp('dbfs:/FileStore/tables/data/YellowTaxis_append.csv','dbfs:/mnt/datalake/book/chapter03/YellowTaxis_append.csv')
+except:
+    print("ファイルが存在しません。dbfs:/FileStore/tables/dataへYellowTaxis_append.csvファイルをアップロードしてください。")
+
 # COMMAND ----------
 
 # MAGIC %md
@@ -102,102 +110,3 @@ except:
 dbutils.fs.rm("/mnt/datalake/book/chapter03/YellowTaxis_append.csv", recurse=True)
 
 # COMMAND ----------
-
-# Spark DataFrameを使ったデータの作成
-data = [
-    {
-        'RideId': 9999996,
-        'VendorId': 1,
-        'PickupTime': '2019-01-01T00:00:00.000Z',
-        'DropTime': '2022-01-01T00:13:13.000Z',
-        'PickupLocationId': 170,
-        'DropLocationId': 140,
-        'CabNumber': 'TAC399',
-        'DriverLicenseNumber': 5131685,
-        'PassengerCount': 1,
-        'TripDistance': 2.9,
-        'RateCodeId': 1,
-        'PaymentType': 1,
-        'TotalAmount': 15.3,
-        'FareAmount': 13.0,
-        'Extra': 0.5,
-        'MtaTax': 0.5,
-        'TipAmount': 1.0,
-        'TollsAmount': 0.0,
-        'ImprovementSurcharge': 0.3
-    },
-    {
-        'RideId': 9999997,
-        'VendorId': 1,
-        'PickupTime': '2019-01-01T00:00:00.000Z',
-        'DropTime': '2022-01-01T00:09:21.000Z',
-        'PickupLocationId': 161,
-        'DropLocationId': 68,
-        'CabNumber': 'T489328C',
-        'DriverLicenseNumber': 5076150,
-        'PassengerCount': 1,
-        'TripDistance': 1.1,
-        'RateCodeId': 1,
-        'PaymentType': 2,
-        'TotalAmount': 9.8,
-        'FareAmount': 8.5,
-        'Extra': 0.5,
-        'MtaTax': 0.5,
-        'TipAmount': 0.0,
-        'TollsAmount': 0.0,
-        'ImprovementSurcharge': 0.3
-    },
-    {
-        'RideId': 9999998,
-        'VendorId': 1,
-        'PickupTime': '2019-01-01T00:00:00.000Z',
-        'DropTime': '2022-01-01T00:09:15.000Z',
-        'PickupLocationId': 141,
-        'DropLocationId': 170,
-        'CabNumber': 'T509308C',
-        'DriverLicenseNumber': 5067782,
-        'PassengerCount': 0,
-        'TripDistance': 1.7,
-        'RateCodeId': 1,
-        'PaymentType': 1,
-        'TotalAmount': 12.35,
-        'FareAmount': 9.0,
-        'Extra': 0.5,
-        'MtaTax': 0.5,
-        'TipAmount': 2.05,
-        'TollsAmount': 0.0,
-        'ImprovementSurcharge': 0.3
-    },
-    {
-        'RideId': 9999999,
-        'VendorId': 1,
-        'PickupTime': '2019-01-01T00:00:00.000Z',
-        'DropTime': '2022-01-01T00:10:01.000Z',
-        'PickupLocationId': 161,
-        'DropLocationId': 68,
-        'CabNumber': 'VG354',
-        'DriverLicenseNumber': 5012911,
-        'PassengerCount': 2,
-        'TripDistance': 2.86,
-        'RateCodeId': 1,
-        'PaymentType': 1,
-        'TotalAmount': 18.17,
-        'FareAmount': 14.5,
-        'Extra': 0.5,
-        'MtaTax': 0.5,
-        'TipAmount': 2.37,
-        'TollsAmount': 0.0,
-        'ImprovementSurcharge': 0.3
-    }
-]
-
-
-# データフレーム作成
-df = spark.createDataFrame(data)
-
-# CSVファイルに出力
-try:
-    df.write.csv("/mnt/datalake/book/chapter03/YellowTaxis_append.csv", header=True, mode="overwrite")
-    print("CSVファイルが正常に出力されました: /mnt/datalake/book/chapter03/YellowTaxis_append.csv")
-except Exception as e:
-    print("エラーです" ,e)
